d402dce1fcd572364034985ba2f634d447464b5e
Art4 <art4@wlabs.de>
2018-03-13 23:29:17 +0100

    Add Fontawesome 5

--- source/js/index.js
+++ source/js/index.js
@@ -1,2 +1,4 @@
 import 'bootstrap';
+import '@fortawesome/fontawesome-free-webfonts/scss/fontawesome.scss';
+import '@fortawesome/fontawesome-free-webfonts/scss/fa-solid.scss';
 import '../css/style.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,18 @@ module.exports = {
 						loader: 'sass-loader'
 					}],
 				}),
+			}, {
+				test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url-loader?limit=10000&mimetype=application/font-woff'
+			}, {
+				test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url-loader?limit=10000&mimetype=application/octet-stream'
+			}, {
+				test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'file-loader'
+			}, {
+				test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+				loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
 			},
 		]
 	},
